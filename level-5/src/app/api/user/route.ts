1ce2561a80ee9d3358d977f9c95cd7a9730535a6
Zain Murtaza <zainmurtaza3532@gmail.com>
2026-04-15 16:01:45 +0500

    learn level-5 api method

---
+++ level-5/src/app/api/user/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse  } from "next/server";
+
+export async function GET() {
+    return NextResponse.json({message: "Hello World"});
+}
+
+export async function POST(request: Request) {
+    const body = await request.json();
+    return NextResponse.json({message: `Hello ${body.name}`});
+}
+
+export async function PUT(request: Request) {
+    const body = await request.json();
+    return NextResponse.json({message: `Hello ${body.name}`});
+}
+
+export async function DELETE(request: Request) {
+    const body = await request.json();
+    return NextResponse.json({message: `Hello ${body.name}`});
+}
